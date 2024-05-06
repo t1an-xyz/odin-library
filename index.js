9bd84b35deb184f235a0d025df309f4a624848cb
Theo <62950302+t1an-xyz@users.noreply.github.com>
2024-05-06 13:51:47 -0400

    Library project

---
+++ index.js
@@ -0,0 +1,69 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
+
+    if (title === '' || author === '' || pages === '') {
+        alert('Please fill in all fields');
+        return;
+    }
+
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    const books = document.getElementById('books');
+    books.innerHTML = '';
+    myLibrary.forEach(book => {
+        const bookElement = document.createElement('div');
+        const title = document.createElement('h3');
+        title.innerText = book.title;
+        const author = document.createElement('p');
+        author.innerText = book.author;
+        const pages = document.createElement('p');
+        pages.innerText = book.pages;
+
+        const read = document.createElement('div');
+        read.innerText = book.read ? 'Read' : 'Not read yet';
+        read.addEventListener('click', () => {
+            book.read = !book.read;
+            displayBooks();
+        });
+        read.classList.add('read');
+        const toolTip = document.createElement('span');
+        toolTip.innerText = 'Click to toggle read status';
+        toolTip.classList.add('tooltiptext');
+        read.appendChild(toolTip);
+
+        const removeBook = document.createElement('button');
+        removeBook.innerText = '×';
+        removeBook.addEventListener('click', () => {
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+            displayBooks();
+        });
+        removeBook.classList.add('remove');
+        bookElement.appendChild(removeBook);
+        bookElement.appendChild(title);
+        bookElement.appendChild(author);
+        bookElement.appendChild(pages);
+        bookElement.appendChild(read);
+        bookElement.classList.add('book');
+        books.appendChild(bookElement);
+    });
+}
+
+document.getElementById('submit').addEventListener('click', () => {
+    addBookToLibrary();
+    displayBooks();
+});
