3f4744cb920eec3f3c1eaf4c44ba5e7836798ae1
Theo <62950302+t1an-xyz@users.noreply.github.com>
2024-05-10 14:34:34 -0400

    Replace constructor with class

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    title;
+    author;
+    pages;
+    read;
+
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary() {
